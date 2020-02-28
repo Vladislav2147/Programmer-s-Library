eeafa350ba16bb6cf06af66cd8d950347aee1a05
Vladislav2147 <vandl3511@gmail.com>
2020-02-28 11:23:27 +0300

    added library database

--- src/main/java/com/ShichkoVlad/Program.java
+++ src/main/java/com/ShichkoVlad/Program.java
@@ -1,99 +1,45 @@
 //TODO
 // Вернуть логгер
-// Снести коллекции + библиотеку
-// Добавить класс работы с бд (добавить, удалить, поиск, редактировать). В метод передается объект и имя таблица, возвращает успешность операции
+// Снести коллекции + библиотеку, менджеров
+// Возможно классы для работы с каждой таблицей (типо writers) и/или обобщение
+// Добавить id в классы
+// Добавить менеджер работы с бд (добавить, удалить, поиск, редактировать). В метод передается объект и имя таблицы, возвращает успешность операции
 // Редактирование: выполнить поиск по имени + заменить на новый объект
 // Вызывать его методы в менеджерах
 package com.ShichkoVlad;
 
 import com.ShichkoVlad.Book.Book;
+import com.ShichkoVlad.Book.Country;
 import com.ShichkoVlad.Exceptions.*;
 import com.ShichkoVlad.Library.*;
 import com.ShichkoVlad.Reader.Reader;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import java.sql.*;
 import java.time.Year;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Program {
     public static void main (String[] args) {
+        ResourceBundle resource = ResourceBundle.getBundle("db",
+                Locale.getDefault());
+        String url = resource.getString("db.url");
+        String user = resource.getString("db.user");
+        String pass = resource.getString("db.password");
+        try(Connection connect = DriverManager.getConnection(url, user, pass)) {
+            PreparedStatement statement = connect.prepareStatement("SELECT * FROM books");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String str = resultSet.getString("title");
+                if(str.contains("The_book")) {
+                    System.out.println(str);
+                }
 
-        PropertyConfigurator.configure("resources/log4j.properties");
-        Logger logger = Logger.getLogger(Program.class);
-
-        Library library = new Library();
-
-        BookManager bookManager = new BookManager(library);
-
-        //bookManager.addNewBook(new Book("The first book", Year.of(1997)));
-        //bookManager.addNewBook(new Book("The second book", Year.of(1999)));
-
-        //bookManager.writeListToFile("books.bin");
-        bookManager.getListFromFile("books.bin");
-
-        System.out.println("\nСписок книг, полученный из файла:");
-        for(Book book: library.getBooks()) {
-            System.out.println(book);
-        }
-
-        ReaderManager readerManager = new ReaderManager(library);
-
-        //readerManager.addNewReader(new Reader("Vlad", "Shichko", "vandl3511@gmail.com"));
-        //readerManager.addNewReader(new Reader("Name", "Surname", "noname@gmail.com"));
-
-        //readerManager.writeListToFile("readers.bin");
-        readerManager.getListFromFile("readers.bin");
-
-        System.out.println("\nСписок читателей, полученный из файла:");
-        for(Reader reader: library.getReaders()) {
-            System.out.println(reader);
-        }
-
-        try {
-            System.out.println("\nКнига с названием \"The first book\":");
-            System.out.println(bookManager.findByBookName("The first book"));
-        }
-        catch (NoSuchBookException e) {
-            System.out.println(e.getMessage());
-            logger.error(e);
-        }
-
-        try {
-            Reader myReader = readerManager.findByEmail("vandl3511@gmail.com");
-            Book myBook = library.getBooks().get(0);
-            bookManager.giveBookToReader(myBook, myReader);
-
-            System.out.println("\nВыдали читателю книгу: ");
-            System.out.println(myReader);
-
-            System.out.println("\nПытаемся выдать ту же книгу другому читателю -> получаем исключение: ");
-            bookManager.giveBookToReader(myBook, library.getReaders().get(1));
-
-        }
-        catch (NoSuchReaderException | NoSuchBookException | ReaderAlreadyHasBookException e) {
-            System.out.println(e.getMessage());
-            logger.error(e);
-        }
-
-        try {
-            System.out.println("\nВозвращаем книгу:");
-
-            Reader myReader = readerManager.findByEmail("vandl3511@gmail.com");
-            bookManager.takeBookFromReader(myReader);
-
-            System.out.println("Книга: " + myReader.getBook());
-
-            System.out.println("\nУдаляем читателя и выводим список читателей: ");
-            readerManager.removeReader(myReader);
-
-            for(Reader reader: library.getReaders()) {
-                System.out.println(reader);
             }
+        } catch(SQLException e) {
+            System.out.println(e);
         }
-        catch (NoSuchReaderException | NoSuchBookException e) {
-            System.out.println(e.getMessage());
-            logger.error(e);
-        }
-
     }
 }
