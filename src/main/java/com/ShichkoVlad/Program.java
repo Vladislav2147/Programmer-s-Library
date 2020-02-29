c95b693dc5d37638a7b9893a034017a2f41169f9
Vladislav2147 <vandl3511@gmail.com>
2020-03-01 00:24:28 +0300

    finished database managers

--- src/main/java/com/ShichkoVlad/Program.java
+++ src/main/java/com/ShichkoVlad/Program.java
@@ -4,16 +4,11 @@
 // Вызывать его методы в менеджерах
 package com.ShichkoVlad;
 
-import com.ShichkoVlad.Book.Author;
-import com.ShichkoVlad.Book.Country;
-import com.ShichkoVlad.Book.Gender;
-import com.ShichkoVlad.Book.Publisher;
-import com.ShichkoVlad.DatabaseManagers.DatabaseAuthorManager;
-import com.ShichkoVlad.DatabaseManagers.DatabasePublisherManager;
-import com.ShichkoVlad.Exceptions.*;
+import com.ShichkoVlad.Book.Book;
+import com.ShichkoVlad.DatabaseManagers.BookTableManager;
+import com.ShichkoVlad.Exceptions.AmbiguousFilterException;
 
 import java.sql.*;
-import java.time.Year;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -26,14 +21,10 @@ public class Program {
         String pass = resource.getString("db.password");
 
         try(Connection connect = DriverManager.getConnection(url, user, pass)) {
-
-            //Publisher publisher = new Publisher(4, "some publisher", Country.Belarus, "8 lenina st., Minsk", "220001", "some@tut.by");
-            DatabasePublisherManager publisherManager = new DatabasePublisherManager();
-            //publisherManager.addToTable(publisher, connect);
-            System.out.println(publisherManager
-                    .getInstanceById(publisherManager.getIdByFilter("name = 'some publisher'", connect), connect));
-            publisherManager.changeInTable(4, "name = 'some_publisher'", connect);
-
+            BookTableManager manager = new BookTableManager();
+            Book book = manager.getInstanceById(1, connect);
+            manager.removeFromTable(1, connect);
+            manager.addToTable(book, connect);
         } catch(SQLException | AmbiguousFilterException e) {
 
             System.out.println(e);
